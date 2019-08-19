69f141803ab6cea93629d248fdd12c07370ea76f
marcinjak9 <marcinjak9@gmail.com>
2019-08-19 19:46:52 +0200

    debugging function

--- src/callback.js
+++ src/callback.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   if (event.httpMethod === 'POST') {
-    console.log(event.body)
+    console.log(event)
     console.log(JSON.parse(event.body))
     callback(null, {
       statusCode: 200,
